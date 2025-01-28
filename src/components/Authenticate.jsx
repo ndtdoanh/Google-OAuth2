c4aee7fb5d2d32dc07e845700b66ea199f40d635
doanh <nguyentrongdoanh202@gmail.com>
2025-01-29 01:31:46 +0700

    implement authentication & authorization code

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,29 +1,42 @@
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { setToken } from "../services/localStorageService";
-import { Box, CircularProgress, Typography } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { setToken } from '../services/localStorageService';
+import { Box, CircularProgress, Typography } from '@mui/material';
 
 export default function Authenticate() {
   const navigate = useNavigate();
   const [isLoggedin, setIsLoggedin] = useState(false);
 
   useEffect(() => {
-    const accessTokenRegex = /access_token=([^&]+)/;
-    const isMatch = window.location.href.match(accessTokenRegex);
+    console.log(window.location.href);
+
+    const authCodeRegex = /code=([^&]+)/;
+    const isMatch = window.location.href.match(authCodeRegex);
 
     if (isMatch) {
-      const accessToken = isMatch[1];
+      const authCode = isMatch[1];
 
-      console.log("Token: ", accessToken);
+      fetch(
+        `http://localhost:8080/api/v1/auth/outbound/authentication?code=${authCode}`,
+        {
+          method: 'POST',
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
 
-      setToken(accessToken);
-      setIsLoggedin(true);
+          setToken(data.result?.token);
+          setIsLoggedin(true);
+        });
     }
   }, []);
 
   useEffect(() => {
     if (isLoggedin) {
-      navigate("/");
+      navigate('/');
     }
   }, [isLoggedin, navigate]);
 
@@ -31,12 +44,12 @@ export default function Authenticate() {
     <>
       <Box
         sx={{
-          display: "flex",
-          flexDirection : "column",
-          gap: "30px",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '30px',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
         }}
       >
         <CircularProgress></CircularProgress>
--- src/configurations/configuration.js
+++ src/configurations/configuration.js
@@ -1,5 +1,6 @@
 export const OAuthConfig = {
-  clientId: '<clientId>',
+  clientId:
+    '626901727884-1tjqf1ktfgb32384892hu1dd2360dg0h.apps.googleusercontent.com',
   redirectUri: 'http://localhost:3000/authenticate',
   authUri: 'https://accounts.google.com/o/oauth2/auth',
 };
