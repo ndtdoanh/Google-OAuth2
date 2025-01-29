21369eecbb01e5fbfaf6f0b4ecbd9faf6d6b8c73
doanh <nguyentrongdoanh202@gmail.com>
2025-01-30 05:07:14 +0700

    update code base

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,26 +2,85 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getToken } from '../services/localStorageService';
 import Header from './header/Header';
-import { Box, Card, CircularProgress, Typography } from '@mui/material';
+import {
+  Alert,
+  Box,
+  Button,
+  Card,
+  CircularProgress,
+  Snackbar,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 export default function Home() {
   const navigate = useNavigate();
   const [userDetails, setUserDetails] = useState({});
+  const [password, setPassword] = useState('');
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState('');
+  const [snackType, setSnackType] = useState('error');
+
+  const handleCloseSnackBar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackBarOpen(false);
+  };
+
+  const showError = (message) => {
+    setSnackType('error');
+    setSnackBarMessage(message);
+    setSnackBarOpen(true);
+  };
+
+  const showSuccess = (message) => {
+    setSnackType('success');
+    setSnackBarMessage(message);
+    setSnackBarOpen(true);
+  };
 
   const getUserDetails = async (accessToken) => {
-    const response = await fetch(
-      'http://localhost:8080/identity/users/my-info',
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+    const response = await fetch('http://localhost:8080/api/v1/users/myInfo', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
     const data = await response.json();
 
     console.log(data.result);
+
     setUserDetails(data.result);
+  };
+
+  const addPassword = (event) => {
+    event.preventDefault();
+
+    const body = {
+      password: password,
+    };
+    fetch('http://localhost:8080/api/v1/users/create-password', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${getToken()}`,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.code != 1000) throw new Error(data.message);
+        getUserDetails(getToken());
+        showSuccess(data.message);
+      })
+      .catch((error) => {
+        showError(error.message);
+      });
   };
 
   useEffect(() => {
@@ -37,6 +96,21 @@ export default function Home() {
   return (
     <>
       <Header></Header>
+      <Snackbar
+        open={snackBarOpen}
+        onClose={handleCloseSnackBar}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <Alert
+          onClose={handleCloseSnackBar}
+          severity={snackType}
+          variant="filled"
+          sx={{ width: '100%' }}
+        >
+          {snackBarMessage}
+        </Alert>
+      </Snackbar>
       {userDetails ? (
         <Box
           display="flex"
@@ -51,7 +125,7 @@ export default function Home() {
               minWidth: 400,
               maxWidth: 500,
               boxShadow: 4,
-              borderRadius: 4,
+              borderRadius: 2,
               padding: 4,
             }}
           >
@@ -63,7 +137,7 @@ export default function Home() {
                 width: '100%', // Ensure content takes full width
               }}
             >
-              <p>Welcome back to ndtdoanh,</p>
+              <p>Welcome back to hacof, {userDetails.username}</p>
               <h1 className="name">{`${userDetails.firstName} ${userDetails.lastName}`}</h1>
               <p className="email">{userDetails.dob}</p>
               <ul>
@@ -74,6 +148,38 @@ export default function Home() {
                   </li>
                 ))}
               </ul>
+              {userDetails.noPassword && (
+                <Box
+                  component="form"
+                  onSubmit={addPassword}
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px',
+                    width: '100%',
+                  }}
+                >
+                  <Typography>Do you want to create password?</Typography>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    fullWidth
+                  >
+                    Create password
+                  </Button>
+                </Box>
+              )}
             </Box>
           </Card>
         </Box>
